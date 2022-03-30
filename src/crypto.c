ad767de93bfacddc01574504f27beb2f2eebd593
Richard Blundell <none@none.com>
2022-03-30 22:58:03 +0000

    automated git commit

--- src/crypto.c
+++ src/crypto.c
@@ -8,11 +8,18 @@
 /***********************/
 #include "../include/string.h"
 /***********************/
-#include "../deps/b64/buffer.c"
-#include "../deps/b64/decode.c"
-#include "../deps/b64/encode.c"
-/***********************/
-
+#include "./deps/c_aes/aes.c"
+#include "./deps/c_aes/aes_change.c"
+#include "./deps/c_aes/aes_decrypt.c"
+#include "./deps/c_aes/aes_encrypt.c"
+#include "./deps/c_aes/aes_key_expansion.c"
+#include "./deps/c_aes/aes_padding.c"
+#include "./deps/c_aes/aes_random.c"
+#include "./deps/c_aes/aes_tools.c"
+#include "./deps/c_aes/matrix_add_round_key.c"
+#include "./deps/c_aes/matrix_mix_columns.c"
+#include "./deps/c_aes/matrix_shift_rows.c"
+#include "./deps/c_aes/matrix_sub_bytes.c"
 
 /***********************/
 
