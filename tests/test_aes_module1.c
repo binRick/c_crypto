656c1af011530fa9cfc96a9c2c7027d0592adb0c
Richard Blundell <none@none.com>
2022-03-31 01:35:22 +0000

    m

--- tests/test_aes_module1.c
+++ tests/test_aes_module1.c
@@ -2,8 +2,11 @@
 #include <stdio.h>
 #include <string.h>
 /*******************/
-#define MODULE_TEST_ENABLED    false
+#define MODULE_TEST_ENABLED    true
 #define MODULE_EXEC_ENABLED    false
+#define MODULE_TEST_GET_KEY    true
+#define MODULE_TEST_SET_KEY    true
+#define MODULE_TEST_AES_KEY    "0123456789abcde1"
 #define MODULE_LOG_LEVEL       LOG_TRACE
 /*******************/
 #include "include/module.h"
@@ -12,51 +15,84 @@
 /*******************/
 #include "src/aes_module.c"
 /*******************/
+#define DEFAULT_CRYPTO_AES_KEY    "0123456789abcdef"
+/*******************/
+#define LOG                       log_debug
+#define LOG_AES_STYLE             AC_RESETALL AC_REVERSED AC_BLUE AC_BOLD
+/*******************/
+#define set_aes_key               cm_crypto->setkey
+#define get_aes_key               cm_crypto->getkey
+/*******************/
+#define PRINT_AES_KEY()    {                                           \
+    char *k = get_aes_key();                                           \
+    LOG(LOG_AES_STYLE "%db Current Key:%s" AC_RESETALL, strlen(k), k); \
+}                                                                      \
+/*******************/
+void cm_print_key(){
+  PRINT_AES_KEY();
+}
 
 
+void cm_set_key(char *s){
+  int res = set_aes_key(s);  LOG(LOG_AES_STYLE "AES Key Set from %db to %db string: %d" AC_RESETALL, strlen(s), strlen(get_aes_key()), res);
+}
+/*******************/
+#define SET_AES_KEY(NEW_KEY)      { \
+    cm_set_key(NEW_KEY);            \
+}                                   \
+/*******************/
+#define IF_TEST_EXEC(IF, EXEC)    { \
+    if (IF) { EXEC(); }             \
+}                                   \
+/*******************/
+#define POST_MODULE \
+  post();           \
+/*******************/
+#define PRE_MODULE \
+  pre();           \
+/*******************/
+/*******************/
+#define MODULE_TEST_SET_KEY_HANDLER()    { \
+    SET_AES_KEY(MODULE_TEST_AES_KEY);      \
+}                                          \
+/*******************/
+#define TEST_MODULE                                                \
+  if (MODULE_TEST_ENABLED) {                                       \
+    test();                                                        \
+    IF_TEST_EXEC(MODULE_EXEC_ENABLED, exec)                        \
+    IF_TEST_EXEC(MODULE_TEST_SET_KEY, MODULE_TEST_SET_KEY_HANDLER) \
+    IF_TEST_EXEC(MODULE_TEST_GET_KEY, PRINT_AES_KEY)               \
+  }                                                                \
+/*******************/
+////////////////////////////////////////////////////////////////////
+//         char * GET_AES_KEY();
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+//         int SET_AES_KEY(char *s);
+////////////////////////////////////////////////////////////////////
+/*******************/
+#define MODULE_HOOK                    \
+  PRINT_AES_KEY();                     \
+  SET_AES_KEY(DEFAULT_CRYPTO_AES_KEY); \
+  PRINT_AES_KEY();                     \
+/*******************/
+#define MODULE_LIFECYCLE \
+  PRE_MODULE             \
+  MODULE_HOOK            \
+  TEST_MODULE            \
+  POST_MODULE            \
+/*******************/
 void init(){
   log_set_level(MODULE_LOG_LEVEL);
 }
 
 
-void module_hook(){
-  char *cur_key; bool ok;
-
-
-  void    do_setkey(char *s){
-    ok = cm_crypto->setkey(s);
-    log_info("key set:%d", ok);
-  }
-
-
-  void    do_getkey(){
-    cur_key = cm_crypto->getkey();
-    log_info("cur key:%s", cur_key);
-  }
-
-  do_getkey();
-  do_setkey("0123456789abcdef");
-  do_getkey();
-} /* module_hook */
-
-
 /*******************/
-void module_lifecycle(){
-  pre();
-  if (MODULE_TEST_ENABLED) {
-    test();
-  }
-  if (MODULE_EXEC_ENABLED) {
-    exec();
-  }
-  module_hook();
-  post();
-}
 
 
 int main(void) {
   init();
-  module_lifecycle();
+  MODULE_LIFECYCLE
   return(0);
 }
 /*******************/
