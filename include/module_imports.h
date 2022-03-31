2eb4efa80a112da85451dedd35b424e63a3cb951
Richard Blundell <none@none.com>
2022-03-31 03:38:20 +0000

    automated git commit

--- include/module_imports.h
+++ include/module_imports.h
@@ -1,3 +1,4 @@
 /*******************/
-#include "./deps/c_aes/aes.h"
+#include "deps/c_aes/aes.h"
+#include "deps/c_list/list.h"
 /*******************/
---
+++ tests/test_list1.c
@@ -0,0 +1,76 @@
+#include "deps/c_list/list.h"
+#include "log/log.c"
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+  int         key;
+  struct node *next;
+} node_t;
+
+node_t *l0 = NULL;
+
+
+int cmp1(node_t *a, node_t *b){
+  return(a->key - b->key);
+}
+
+
+int cmp2(node_t *a, node_t *b){
+  return(b->key - a->key);
+}
+
+#define LIST_APPEND     SL_APPEND
+#define LIST_PREPEND    SL_PREPEND
+#define LIST_FOREACH    SL_FOREACH
+
+#define LIST_ACTION_COMMON          \
+  node_t *i;                        \
+  i       = malloc(sizeof(node_t)); \
+  i->key  = k;                      \
+  i->next = NULL;                   \
+
+#define POPULATE_LIST \
+  APPEND_ITEM(10);    \
+  APPEND_ITEM(5);     \
+  APPEND_ITEM(200);   \
+  PREPEND_ITEM(5);    \
+  PREPEND_ITEM(1);    \
+  APPEND_ITEM(169);   \
+
+#define DO_PRINT_LIST()    {                    \
+    node_t *ti; int i = 0;                      \
+    LIST_FOREACH(l0, ti) {                      \
+      log_debug("key #%d: %d", i + 1, ti->key); \
+      i++;                                      \
+    }                                           \
+}                                               \
+
+void PREPEND_ITEM(int k){
+  LIST_ACTION_COMMON
+  LIST_PREPEND(l0, i);
+}
+
+
+void APPEND_ITEM(int k){
+  LIST_ACTION_COMMON
+  LIST_APPEND(l0, i);
+}
+
+
+int main(){
+  log_debug("populating.....");
+  POPULATE_LIST
+  DO_PRINT_LIST();
+
+  log_debug("sorting.....");
+  SL_SORT(l0, cmp1);
+  DO_PRINT_LIST();
+  log_debug("sorted.....");
+
+  log_debug("sorting.....");
+  SL_SORT(l0, cmp2);
+  DO_PRINT_LIST();
+  log_debug("sorted.....");
+  return(0);
+}
